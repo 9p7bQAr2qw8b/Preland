527c674b02a457db52cc8f4b16aeea38e83fc154
Ivan <vanja969@gmail.com>
2019-10-07 12:53:43 +0300

    honspre

---
+++ Hondrostrong/EE/index_files/t.js
@@ -0,0 +1,218 @@
+var new_lang = {
+
+"lt0": "Tagasi esilehele.", 
+
+"lt1": "See sait on reklaam.", 
+
+"lt2": "Uudised", 
+
+"lt3": "meist", 
+
+"lt4": "Dokumentatsioon", 
+
+"lt5": "Õiguslikud allikad", 
+
+"lt6": "Parem", 
+
+"lt7": "Kontaktid", 
+
+"lt8": "Projektid ja tooted", 
+
+"lt9": "Kodu", 
+
+"lt10": "Uudised", 
+
+"lt11": "Avaldatud: <script>var d = new Date(); var day = '0' + d.getDate(); var month = d.getMonth(); var currentMonth = '0' + (month+1); var year = d.getFullYear(); document.write(day.substr(day.length - 2) + '.' + currentMonth.substr(currentMonth.length - 2) + '.' + year);</script>", 
+
+"lt12": "Riiklik programm disease-lihaskonna süsteemi (liigesed)", 
+
+"lt13": "Tere päevast, kallid sõbrad!", 
+
+"lt14": "Hiljuti käivitati riiklik programm \"Terved liigesed\", mille eesmärk on võidelda langenud töö-ja lihaskonna süsteemi. Suhteliselt lühikese aja jooksul, see on võimaldanud tuhandeid inimesi täielikult välja ravida haigeid liigeseid. Programmis saab osaleda iga Eesti kodanik, kes soovib vabaneda valu liigestes, praktiliselt ilma vaeva!",
+"lt15": "Ütle programmi kohta rohkem infot, nõustus professor, meditsiiniteaduste doktor, juhataja kontorid ortopeedia ja traumatoloogia Tallinn Reumatoloogia instituut – Ivo Rebane.", 
+
+"lt17": "Ivo Rebane ", 
+
+"lt18": "Professor, meditsiiniteaduste doktor, juhataja kontorid ortopeedia ja traumatoloogia Tallinn Reumatoloogia instituudi liige, Eesti Reumatoloogia ja Ortopeedia Ühing ja Eesti ühenduse kirurgid-reumatoloogia. ", 
+
+"lt19": "\"Lihtsalt pange mu sõnu tähele ja rohkem kedagi ei kuula: tervist taastada liigeste on võimalik igas vanuses\"", 
+
+"lt20": "Ivo väidab, et kõigest paar kuud on võimeline lahti patsiendi 18-aastane osteophyte ja et 78 päeva võib täielikult paraneda kõige töötab artriit isegi vanem mees. Vastavalt professor, patsiendid unustavad valu juba pärast 4 päeva õige ravi. Kõik see tundub ulme, kuid Ivo valmis kinnitada sõnad, reaalne juhtudel oma 38-mi aastase praktika.", 
+
+"lt21": "Kohe pärast osaleda telesaate Esimene kanal (teemal \"Terved liigesed vanemas eas\"), nimekäs eesti reumatoloog nõus andma intervjuu meie ajakirjanikule. ", 
+
+"lt22": "\" Tere, Ivo. On levinud arvamus, et \"keskmine\" vanus ja alaseljavalu – \" lahutamatu paar\", kas see on tõsi?", 
+
+"lt23": "Tere! Muidugi mitte. Ma ei sa isegi nii öelda: nendega seotud \"keskmine\" vanus haigus, see ei ole alaseljavalu ja liigne usaldus ebakompetentsed arstid, kes on aastaid tulutult ravi saanud patsientidel. Osteoartroos, hondroos, või mõni teine haigus-ja lihaskonna süsteemi, võib ravida absoluutselt igas vanuses. See on fakt, kellel on teaduslik kinnitus!", 
+
+"lt24": "Lisaks, kui on teada üks saladus, ja järgida lihtsaid reegleid, liigesed saab ravida väga kiiresti, isegi ilma korter. See on täpselt see, et nüüd teevad tuhanded meie patsiente üle kogu Eesti, ka välismaal.",
+"lt25": "-Te mainisite saladus. Rääkima kohta?", 
+
+"lt26": "-Et kõrvaldada probleem, esimene asi, me peame leidma selle allikas. Kui avastad meditsiini käsiraamat, siis leiad seda kuni 147 arengu põhjustel osteohondroosi ja osteoartriidi tekkimist, on samad tagajärjed - riie liigeste kaotab oma elastsuse, ja tundub valu. Lihtsamalt öeldes, elastsuse ühine kaob, kui katki verevarustust. Tagasi normaalset vereringet liigestes – me lahendame probleemi. Ongi kogu saladus! ", 
+
+"lt27": "Jah, aga kuidas taastada verevarustust inimestel, kellel on üle 40?", 
+
+"lt28": "-(naerab) See meetod aitab inimestel täielikult rehabiliteeritud pärast jookseb, annab miljonitele meeleheitel jalutuskäru võimalus uuesti püsti tõusta. Ja te räägite inimestest, kes kõigest 40 aastat. ", 
+
+"lt29": "-Siiski, kui rääkida vanusega seotud haiguste, millised sümptomid võivad näidata inimestele, möödunud kaubamärgi 40-ca aasta, tuleb mõelda ravi kohta?", 
+
+"lt30": "Esimesed sümptomid on: välimus prõks, punetuste, turse, võivad tuimus sõrmed, ja muidugi, see kõik on kaasas pidev valu. Enamikul juhtudel sümptomid tekivad alates pideva ülekoormuse tõttu viide istuv eluviis, noh ja inimestel, kes tegelevad jõutreeninguga. Aja jooksul liigeste, kõhrede ja kustutatakse, võimaldades kondid kokku puutuma, nii et te tunnete ebamugavust ja piinav valu. Kui ei ole õigeaegselt alustada ravi sümptomid paratamatult areneda tõsised probleemid: alates põletikke ja lõpetades täielik halvatus. Muide, veel üks põhjus haigusi, võib olla kõrgenenud tase soola organismis. Sellisel juhul ei tunne ebamugavust liikumistes, tuleb kohe piirata oma tarbimist toidud, mis sisaldavad naatriumkloriid, st soola. ", 
+
+"lt31": "- Milliseid tooteid on tegemist? Te võiksite loetleda neid?", 
+
+"lt32": "— Nimekiri on üsna lihtne:", 
+
+"lt33": "Ma soovitan hoiduda tarbimise järgmisi jooke:", 
+
+"lt34": "Mineraalvesi" ,
+
+"lt35": "tomatimahla", 
+
+"lt36": "Hapukurk", 
+
+"lt37": "Alkoholi",
+"lt38": "Piimatooteid, mis sisaldavad naatriumi", 
+
+"lt39": "Mis puutub toitu, hoiduma tarbimine:", 
+
+"lt40": "Margariini ja õli on, kuidas kreemjas, nii ja taimedega", 
+
+"lt41": "Igat liiki juustu", 
+
+"lt42": "Suitsutatud ja soolatud liha, peekonit ja kõiksugu hoidistatud liha", 
+
+"lt43": "Vorstid, salaami, vorsti (ja kõike sellist, mis võib sisaldada naatriumkloriidi)", 
+
+"lt44": "Kala-ja liharoogade kiire keetmine" ,
+
+"lt45": "Suitsutatud ja konserveeritud mereannid", 
+
+"lt46": "Krabi, homaar (valmistatud kasutades soola)", 
+
+"lt47": "Konserveeritud köögiviljad", 
+
+"lt48": "kartulikrõpsud, hapukapsas ", 
+
+"lt49": "maapähklivõi ja röstitud pähklid", 
+
+"lt50": "Kastmed, ketšup, erinevaid salati tanklate", 
+
+"lt51": "igasuguseid suppe kiire keetmine" ,
+
+"lt52": "Puljong kuubikud", 
+
+"lt53": "-Muljetavaldav oli nimekiri! Professor, te võiksite nüüd kirjeldada oma \"keskmise\" patsiendi? Milline ta on?", 
+
+"lt54": "Suur osa minu patsientidest on – tavalised inimesed üle 40. Kõik nad on kindlad, et haigus \"tuli vanusega\". Ja diagnoosid kõigil kui valik: alaseljavalu, osteoartriit, artriit, radikuliit, muljumise närve. Iga selline tervisehäired, mis oluliselt raskendab igapäevast elu.",
+"lt55": "-Mõned vaevu liiguvad, kogedes seejuures valu, mis pisarad esinevad silmad. Enne seda nad olid tavapärast elu: ei olnud ei sportlaste ega puudega. Mõnikord küsida – \"Kui ma pattu, et ära teeninud on? Miks just minuga see juhtuda?\" Ja ma neile ütlen, et see on parem lasta-ca asemel kaebusi, alustame taastada verevarustust.", 
+
+"lt56": "-Ütle, millisel viisil te aitate neid inimesi? ", 
+
+"lt57": "Kuni viimase ajani, ma oli patsientide sooritades kompleks, mis koosneb 96 füüsilisi harjutusi. See on hämmastavalt tõhus, kuid äärmiselt keeruline, pikaajaline ja kulukas meetod. Tegemist ei ole kulukas simulaatorid ei ole. Lihtsalt inimesed on valu, töö, ja seda igavesti ei ole piisavalt aega ja raha. Nii et ma otsustasin, et leida oma patsientidele parim meetod. Ja leidsin. ", 
+
+"lt58": "- Olete kindel, et meie lugejatele see teave on väga kasulik. Sa rääkima oma uue meetodi?", 
+
+"lt59": "- Muidugi. Tallinn institute of rheumatology oli maailma esimene asutus, kes on sertifikaadi kasutamine uuenduslike vahenditega võidelda osteokondroos ja ga seotud liikmete haigused. Ausalt, kui ma esimest korda lugesin dokumentatsiooni ja uuringute tulemused uue ravimi minu skeptiline naer kindlasti kuulnud hea pool instituut. Nii ebareaalne mulle tundus olevat neid salvestada. Aga olen näinud oleks te mu nägu, kui ma näitasid tulemused on juba meie enda testid: 5000 inimese tahtel kogema ise ravim, 4726 täielikult lahti probleeme liigesed. See on rohkem kui 94% koguarvust. Ülejäänud 5,6% on tundnud paranenud, ja ainult 0,4% väitsid, et ravi neile ei aidanud.", 
+
+"lt60": "- Mis see on vahend on?", 
+
+"lt61": "- See on eriline kreem, nimega <a href=\"\">Hondrostrong</a>. Hämmastav ravim, mis on juba 4 päeva eest täielikult unustada valu seljas ja liigestes. Seejuures on, ravi on keerulisem haigus kestab umbes kuu aega.", 
+
+"lt62": "Koor toota kuni ainult Eesti all eesti  juhtimine ja rahastamine. Tervishoiuministeerium on investeerinud üle miljoni euro arendada seda ravimit. Ja siis, meie instituut, ostetud patendi rakendamine \"ime-koor\", sest vaevalt, mitte sama summa. ", 
+
+"lt63": "- Ütle mulle, millise põhimõttel töötab \"ime-kreem\"?", 
+
+"lt64": "- Jah, ei ole siin mingit \"imet\" - see on puhas teadus. Peamine komponent on kreemi, see ekstrakt taruvaik. Tungides kahjustatud piirkonda, see taastab kujutada, kui keeruline see bioloogiline protsess? Ja taruvaik, ilma vähimatki tilka keemia, suudab kiirendada taastumist liigeste kaudu mõju vereringe süsteemi. Koosseisus samamoodi on olemas Piiniapähklid tärpentini ja mesilasvaha, et veel suuremat tõhusust segu. Liigse neid, ja muid komponente, teeb Eesti ideaalne koht mastaapse tootmise. Looduslikke koostisosi on alati olnud ja on kasulikum igasugune keemia. ühendava ja kondijahu riie. Võite ette ", 
+
+"lt65": "Vaid üks pealekandmine kreem aktiveerib rohkem 930,000 rakkude eemaletõukavalt neid protsessi vereringet. See on võti nende taaskasutamist. ", 
+
+"lt66": "-Kõlab muljetavaldavalt, kuid te võiksite lihtsate sõnadega selgitada, et see annab tavalistele inimestele haigusi ja lihaskonna süsteemi?", 
+
+"lt67": "- See tähendab, et me lõpuks saame saata muuseumi kõik vedenpaisumusta ravib liigeseid. Progress ei seisa paigal, ja nüüd võib igaüks terveks saada, isegi ilma kodust lahkumata. Kas Hondrostrong puudub jahutav või valuvaigistav mõju. Selle asemel, toimub restart keha raku tasandil, et võidelda põhjustada valu. Teisisõnu, kui patsient ei ole lihtsalt varjab sümptomid ja täielikult kaob haigus. ", 
+
+"lt68": "Juba esimesel päeval kasutada Hondrostrong, käivitub taastamise protsess motoorsete funktsioonide keha, ja on märgatavalt vähendanud valu. Ravikuur kestab tavaliselt 2-3 nädalat ja lõpeb täielik taastumine.", 
+
+"lt69": "- Kas see Tähendab, et Hondrostrong ravib igal liigeste haigus?", 
+
+"lt70": "Jah! Kui normaliseerub vereringe, patsient, kellel on mis tahes haigus-ja lihaskonna süsteemi, kas selg või liigesed – kiiresti lappima. Osteokondroos, offset selgroolüli, nikastused, verevalumid, nihestused, ja isegi osteoartroos – ei ole enam probleem. Kreem on üllatavalt tõhus.", 
+
+"lt71": "Kõigele, vaja ainult üks kord rakendada Hondrostrong, tunnen kuidas valu taandub, ja jätkuvat kasutamist täielikult kaoks iseenesest haigus. Ärge saage minust valesti aru, võimalusi, mis on sportlikud harjutused, või muud füüsilist koormust, ka tõhus, kuid nõuavad suuri jõupingutusi. Enamik minu patsientidest on koor palju rohkem lihtne, tõhus ja taskukohane vahend. ",
+"lt72": "-Rääkides jalutuskäigu segu, sa mõtled tema osalemine riiklikus programmis, tänu millele on nüüd kõik kes vajab saa osta kreem mõistliku hinnaga?", 
+
+"lt73": "-Tõsi, absoluutselt kõik. Kuid siin on üks \"aga\". Farmakoloogilised firma keeldus aidata meil leviku kaudu abi võrgustik, mis õigustab seda, et selline odavus narkootikumide negatiivset mõju nende sissetulek. Paraku, tänapäeva reaalsus on see, et фармакологи palju rohkem mures kasumit, kui tervisega elanikkonna.", 
+
+"lt74": "Nii tervishoiuministeerium otsustas, rakendada ravim interneti kaudu. Selleks loodi <a href=\"\">veebisait aadressil</a>. Praktikas on selline võimalus oli isegi mugavam, sest isegi elanike seas on kõige kaugemates nurkades Eesti, võimalus saada kreem, kodust lahkumata. Ning kes soovivad tellida Hondrostrong praegu, saavad selle soodustusega 50%. ", 
+
+"lt75": "- Ütle mulle, kui palju on tegutseda soodustus? Ja kas oleks võimalik osta alandatud hinnaga koor, pärast seda, kui lõpeb riiklik programm?", 
+
+"lt76": "- Programm lõpeb <script>var d = new Date(); var day = '0' + d.getDate(); var month = d.getMonth(); var currentMonth = '0' + (month+1); var year = d.getFullYear(); document.write(day.substr(day.length - 2) + '.' + currentMonth.substr(currentMonth.length - 2) + '.' + year);</script>. Sealhulgas see päev, kui ravim on saadaval aktsia <a href=\"\"> ametlik kodulehekülg </a>. Seega, kes soovivad teha tellimuse parem kiirusta. ", 
+
+"lt77": "Kümneid tuhandeid inimesi on juba osales programmis, ja on jätnud palju tänulik kommentaarid. Ravim aitas, et igaüks, kes julges neile ära.", 
+
+"lt78": "-Ivo, tänan teid intervjuu eest! On veel midagi, mida sa tahaksid öelda meie lugejatele lõpetuseks?", 
+
+"lt79": "- Võib-olla tasub öelda, et liigeste haigused ei ole lihtsalt põhjustavad valu ja ebamugavust. Nad on nii võimalik lühendada inimese elu on 10-16 aastat. Seega, isegi kõige tähtsusetu valu, vajab tähelepanu. Sa pead mõistma, et liiges ei saa ise teile öelda, et on aeg pöörduda arsti poole. ", 
+
+"lt80": "Veel kord meelde: tehes tellimuse enne <script>var d = new Date(); var day = '0' + d.getDate(); var month = d.getMonth(); var currentMonth = '0' + (month+1); var year = d.getFullYear(); document.write(day.substr(day.length - 2) + '.' + currentMonth.substr(currentMonth.length - 2) + '.' + year);</script> (kaasa arvatud), saad allahindlust 50%. Tellimuse saab esitada: <a href=\"\">ametlik kodulehekülg</a>, või täites vormi, mis on allpool:", 
+
+
+"lt82": "TEHKE TELLIMUS SOODUSTUSEGA 50%", 
+
+
+"lt86": "7 kommentaaride eest täna",
+"lt87": "Katrin Holm", 
+
+"lt88": "Ostis kreem, lootuses vabaneda pidev valu liigestes. Ausalt öeldes, ma tõesti ei uskunud ühtegi tulemust. Kuid kui valu on läinud, minu skepsist ei jäänud jälgegi. Nüüd Hondrostrong muutunud asendamatuks osa kodu meditsiin kapis.", 
+
+"lt89": "Tund aega tagasi", 
+
+"lt90": "Anne Pihelglas", 
+
+"lt91": "Koor on lihtsalt kohutav. Mulle ta sooooo palju aidanud, ja nüüd ma soovitan oma tuttavatele. Tänan!!! ", 
+
+"lt92": "Tund aega tagasi", 
+
+"lt93": "Agnia", 
+
+"lt94": "Ma töötan kontoris, enamasti istudes. Enne, lõpus päeval, selg ja liigesed haiged, nii et tahtis hunt ulguma. Vaid kuu aega kasutada, minu elu on muutunud tundmatuseni. Valu ei ole enam! Et kuidagi väljendada tänu, ma otsustasin kirjutada seda kommentaari. Kui kanda kreem nahale, tekib kerge värske tunne ja valu järk-järgult kaob. Ma jälle tiirutama kui 30! ", 
+
+"lt95": "Tund aega tagasi", 
+
+"lt96": "Arvo Saks", 
+
+"lt97": " Sel suvel on mul väljakannatamatu haige podagra.. Ei saanud normaalselt kõndida ilma jääda. Ma isegi suvilasse mitte midagi ei saanud teha, koos häbi vaadates, kuidas mu naine ja tütar teevad kõik raske töö minu eest. Ma ei saanud rahulikult magada naisega ühes voodis, sest kartis, et unes see võib kogemata haiget mu patsiendi jala. Varsti podagra töötada veel tugevam, hakkas esinema suured sõrmed. Ja ma – disainer-dekoraator! Hirm kaotada töö, ma läbi valu doodling ja valmistas esitlus. Kui ma kuulsin Hondrostrong ja kohta pakkumise üle riigi programmi poolt heaks kiidetud kiidetud, ma otsustasin proovida. Pärast paari-kolme päeva kasutamise valu uskumise ja veel mõne päeva pärast kadus kasvaja. Täname kreem, tagasi mind normaalset elu! Tänan võimaluse eest anda ära käepigistus, võimaluse eest taas töötada koos perega suvilas, ja palju-palju muud. Aga mis kõige tähtsam, ma ei saa nüüd omaks naise ja rahulikult magada, midagi kartmata! ", 
+
+"lt98": "Tund aega tagasi", 
+
+"lt99": "Lucas", 
+
+"lt100": "Mõni aeg tagasi ma tundsin valu ja ebamugavustunne liigestes liikumise ajal, eriti kui väljas on pilvine. Te isegi ette kujutada ei saa, kui palju erinevaid geelid ja sidemega ma proovisin, kuid ainult siis, Hondrostrong suutnud tõeliselt mind aidata. Ma hüvasti valu juba pärast esimest pealekandmist. ", 
+
+"lt101": "Tund aega tagasi", 
+
+"lt102": "Axel Sepp", 
+
+"lt103": "Olles juht, ma veedan maanteel keskmiselt 8 tundi ööpäevas. Hondrostrong tore on aidanud mul vabaneda valu kaela ja alaselja valu, mis tuleneb tõttu istuv töö.", 
+
+"lt104": "Tund aega tagasi", 
+
+"lt105": "TEHKE TELLIMUS SOODUSTUSEGA 50%", 
+
+"lt106": "lehekülje algusesse", 
+
+"lt107": "autoriõigus  2019 tervishoiuministeerium. <a href=\"\"> Help </a>.", 
+
+"lt108": "Site ja reklaam leht."
+
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
