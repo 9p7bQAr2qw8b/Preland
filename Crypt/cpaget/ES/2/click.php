4532698f181a62923bf27c38466e801cb3f4b6bf
9p7bQAr2qw8b <53088141+9p7bQAr2qw8b@users.noreply.github.com>
2020-03-20 16:56:26 +0300

    11

---
+++ Crypt/cpaget/ES/2/click.php
@@ -0,0 +1,7 @@
+
+				var d = new Date();
+				d.setTime(d.getTime() + 3600 * 24 * 1000);
+				document.cookie = 'uclick=' + encodeURIComponent('2th9ocvr') + '; expires='+d+'; path=/';
+				
+				var tokens = new Object();
+			 tokens.uclick = "2th9ocvr"; 
