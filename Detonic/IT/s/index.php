7e34153cfe72057f048e360ea025c9dc18489552
9p7bQAr2qw8b <53088141+9p7bQAr2qw8b@users.noreply.github.com>
2020-07-23 12:30:42 +0300

    11

--- Detonic/IT/s/index.php
+++
@@ -1,7 +0,0 @@
-
-				var d = new Date();
-				d.setTime(d.getTime() + 3600 * 24 * 1000);
-				document.cookie = 'uclick=' + encodeURIComponent('pmsya5a1bl') + '; expires='+d+'; path=/';
-				
-				var tokens = new Object();
-			 tokens.clickid = "3a93apmg6uqa5vr99f";  tokens.uclick = "pmsya5a1bl"; 
